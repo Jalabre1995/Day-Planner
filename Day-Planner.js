f9b36a42ed448034bf72af90470d03ee5fe22655
Joshua Alabre <57151108+Jalabre1995@users.noreply.github.com>
2020-01-02 21:42:47 -0500

    Day-Planner-v7

--- Day-Planner.js
+++ Day-Planner.js
@@ -1,14 +1,16 @@
 //Javasript Function that wraps everything///
-var textInput = document.getElementById("plan");
+var textInput = document.getElementById("plan1");
 var saveButton = document.getElementById("Save-btn");
 var msgDiv = document.getElementById("msg");
 
 $(document).ready(function() {
+    console.log(renderLastSave)
 
 renderLastSave();
 
+
 function displayMessage(type, message){
-    msgDiv.textContent = message;
+    msgDiv.innerHTML = message;
     msgDiv.setAttribute("class", type);
 }
 
@@ -17,13 +19,14 @@ function renderLastSave() {
     if(text === null) {
         return;
     }
- textInput.textContent = textInput;
+    console.log(text)
+ textInput.value = text;
 
 }
 
     $("#Save-btn").click(function(event) {
         event.preventDefault();
-        var text = document.getElementById("plan").value;
+        var text = document.getElementById("plan1").value;
         if (text === "") {
             displayMessage("text blank");
         } else {
@@ -35,5 +38,4 @@ function renderLastSave() {
     
     );
 });
-
 
